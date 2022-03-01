660c97bcb79ace6f5c609f50fdd02c188d8827df
Esteban Cairol <ecairol@gmail.com>
2022-03-01 16:16:03 -0600

    Converts app into API

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 
 ## About
 
-This CLI receives a URL as parameter and sends it to W3C HTML Validator tool.
-It then process the data in a summary.
-
-```
-t51check http://wordpress --save
-```
-
+This CLI receives a URL as parameter and evaluates the accessiblity issues that page might have.
+It then process the data in a summary and posts it to a P2
 
 ## Installation
 
-Create an .env file with your WordPress API account token
-Run `npm install -g .` to install the CLI globally.
+- Clone this repo with the command:
+    - `git clone git@github.com:a8cteam51/team51-a11y-cli.git`
+	- Then run `cd team51-a11y-cli` to navigate to this directory
+
+- Make sure you have NodeJS installed on you local computer.
+    - Open a Terminal a type `node -v` to verify it's installed
+	- If this throws an error, try running `brew install node`
+
+- Duplicate the file `.env_sample` and rename it to `.env`
+    - This can be done with the Finder, or
+	- via Terminal: `cp .env_sample .env`
+
+- Open this `.env` file and place the WPCOM API Token there
+
+- To install the tool, run `npm install -g .`
 
 
 ## Use
 
 The tool can be accessed with the command `t51check`
+
+```
+t51check http://wordpress.com --save
+```
 
 ### List of commands
  - `t51check [url]` to view results in Terminal
---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const axios = require( 'axios' );
+
+const app = express();
+const log = console.log;
+
+const { 
+    generateSummary,
+    formatTerminalOutput,
+    generateHtmlPost,
+    scrapLinks
+} = require('./bin/utils');
+
+app.listen(3000, () => {
+    log("Server running on port 3000");
+});
+
+app.get("/evaluate", (req, res, next) => {
+    const url = req.query.url;
+    const crawl = req.query.crawl || false;
+
+    if ( ! url ) {
+        res.json("Provide a URL");
+    }
+
+    log( `Evaluating ${url}. Crawl: ${crawl}` );
+    ( async function () {
+        // Scrap given URL to include in inspectURLs
+        let inspectURLs = [ url ];
+        if ( crawl ) {
+            await scrapLinks(url)
+                .then(({ data, response }) => {
+                    let limit = isNaN(crawl) ? false : true;
+                    if ( response.statusCode === 200 ) {
+                        for (let i = 0; i < data.links.length; i++) {
+                            const url = data.links[i].href.replace(/\/$/, ""); // remove trailing slash
+                            if ( inspectURLs.includes( url ) ) {
+                                continue;
+                            }
+                            // Verify it's actually a URL
+                            if ( url.indexOf('http') !== 0 ) {
+                                // TODO: support relative paths
+                                continue;
+                            }
+                            if ( limit && (inspectURLs.length >= parseInt(crawl)) ) {
+                                break;
+                            }
+                                
+                            inspectURLs.push( url );
+                            
+                        }
+                    }
+                });
+        }
+    
+        // Remove duplicates
+        inspectURLs = Array.from(new Set(inspectURLs));
+    
+        log('Evaluating these URLs:', inspectURLs);
+    
+        // Pull data from w3.org
+        const inspectURLPromises = [];
+        inspectURLs.forEach( url => {    
+            inspectURLPromises.push(
+                axios( {
+                    method: 'GET',
+                    url: `https://validator.w3.org/nu/?doc=${ url }&out=json`,
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
+                        'Content-Type': 'text/html; charset=UTF-8'
+                    }
+                } )
+            );
+        } );
+    
+        let responses = await axios.all(inspectURLPromises);
+        const data = {
+            messages : responses.map( response => response.data.messages ).flat()
+        }
+    
+        // Process data to create a summary object
+        const summary = generateSummary( data );
+        const postData = {
+            title: `Check for: ${ url }`,
+            tags: [],
+            content: generateHtmlPost( summary, inspectURLs )
+        }
+    
+        res.json( postData );
+      
+    
+    } )();
+});
